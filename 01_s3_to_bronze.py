9cc86694ab2801d1b1ddf569f41c527101d4f9f2
matsuot@revolvesys.co.jp <matsuot@revolvesys.co.jp>
2026-06-05 02:32:06 +0000

    S3からbronzeテーブル生成までのパイプライン完了

--- 01_s3_to_bronze.py
+++ 01_s3_to_bronze.py
@@ -1,24 +1,29 @@
+# 変数の定義（松尾さんの環境用）
+env = "dev"
 s3_source_path = "s3://databricks-sample-bucket-20260602/car_factory_dev/raw/"
+schema_location = "s3://databricks-sample-bucket-20260602/car_factory_dev/checkpoints/bronze/_schema_metadata"
+checkpoint_location = "s3://databricks-sample-bucket-20260602/car_factory_dev/checkpoints/bronze"
+bronze_data_path = f"s3://databricks-sample-bucket-20260602/car_factory_{env}/database/bronze/car_factory_bronze"
 
-checkpoint_path = "s3://databricks-sample-bucket-20260602/car_factory_dev/checkpoints/bronze/"
-
-database_bronze_path = "s3://databricks-sample-bucket-20260602/car_factory_dev/database/bronze/"
-
-
-
-bronze_stream = (spark.readStream
+# 読み込み側
+bronze_stream = (
+    spark.readStream
     .format("cloudFiles")
     .option("cloudFiles.format", "csv")
-    .option("cloudFiles.inferColumnTypes", "true")
-    .option("cloudFiles.schemaLocation", checkpoint_path + "/bronze")
+    .option("cloudFiles.schemaLocation", schema_location)
+    .option("cloudFiles.partitionColumns", "factory_id")
     .option("header", "true")
-    .load(s3_source_path))
+    .load(s3_source_path)
+)
 
-
-query = (bronze_stream.writeStream
+# 書き込み側
+query = (
+    bronze_stream.writeStream
     .format("delta")
     .outputMode("append")
-    .option("checkpointLocation", checkpoint_path)
-    .trigger(availableNow=True) 
-    .option("path", database_bronze_path)
-    .toTable("car_factory_dev.bronze.car_factory_bronze")) 
+    .option("checkpointLocation", checkpoint_location)
+    .option("mergeSchema", "true")
+    .option("path", bronze_data_path)
+    .trigger(availableNow=True)
+    .toTable(f"car_factory_{env}.bronze.car_factory_bronze")
+)
