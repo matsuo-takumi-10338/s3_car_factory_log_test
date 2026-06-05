22260d337822716b097ac1616f289fb3787377cc
matsuot@revolvesys.co.jp <matsuot@revolvesys.co.jp>
2026-06-05 05:04:38 +0000

    環境変数取得共通モジュール化

--- 01_s3_to_bronze.py
+++ 01_s3_to_bronze.py
@@ -1,19 +1,16 @@
-# 変数の定義（松尾さんの環境用）
-env = "dev"
-s3_source_path = "s3://databricks-sample-bucket-20260602/car_factory_dev/raw/"
-schema_location = "s3://databricks-sample-bucket-20260602/car_factory_dev/checkpoints/bronze/_schema_metadata"
-checkpoint_location = "s3://databricks-sample-bucket-20260602/car_factory_dev/checkpoints/bronze"
-bronze_data_path = f"s3://databricks-sample-bucket-20260602/car_factory_{env}/database/bronze/car_factory_bronze"
+from env_config import EnvConfig
+
+config = EnvConfig(layer="bronze")
 
 # 読み込み側
 bronze_stream = (
     spark.readStream
     .format("cloudFiles")
     .option("cloudFiles.format", "csv")
-    .option("cloudFiles.schemaLocation", schema_location)
+    .option("cloudFiles.schemaLocation", config.get_env_params("schema_location"))
     .option("cloudFiles.partitionColumns", "factory_id")
     .option("header", "true")
-    .load(s3_source_path)
+    .load(config.get_env_params("s3_source_path"))
 )
 
 # 書き込み側
@@ -21,9 +18,9 @@ query = (
     bronze_stream.writeStream
     .format("delta")
     .outputMode("append")
-    .option("checkpointLocation", checkpoint_location)
+    .option("checkpointLocation", config.get_env_params("checkpoint_location"))
     .option("mergeSchema", "true")
-    .option("path", bronze_data_path)
+    .option("path", config.get_env_params("data_path"))
     .trigger(availableNow=True)
-    .toTable(f"car_factory_{env}.bronze.car_factory_bronze")
+    .toTable(config.get_env_params("table_name"))
 )
